e9f61dea20c0f7c0b23e79d0986fc85d90e50a29
rjasi <venusara@gmail.com>
2017-05-06 23:39:14 -0400

    finished calculator

--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+def add(x,y)
+	return x+y
+end
+
+def subtract(x,y)
+	return x - y
+end
+
+def sum(x)
+	ans = 0
+	for i in x
+		ans += i
+	end
+	return ans
+end
+
+
+def multiply(x)
+	sum = 1
+	for i in x
+		sum *=i
+	end
+	return sum
+end
+
+def power (base,exp)
+	if (exp == 0)
+		return 1
+	end 
+
+	return base * power(base,exp - 1)
+end 		
+
+
+def factorial (x)
+	if (x == 1 or x == 0)
+		return 1
+	end
+
+	return x * factorial(x-1)
+end 
