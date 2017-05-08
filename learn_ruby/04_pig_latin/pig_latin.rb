5f9ee062faa2cbdf0e99ea8d983346b0a8ca1030
rjasi <venusara@gmail.com>
2017-05-08 02:43:57 -0400

    Finished pig latin

--- learn_ruby/04_pig_latin/pig_latin.rb
+++ learn_ruby/04_pig_latin/pig_latin.rb
@@ -1,1 +1,48 @@
 #write your code here
+def doesQUexist(x,i)
+	if (x[0..i].include? "qu")
+		return x[i+1..-1] +x[0..i] + "ay" 
+	else 
+		return x[i..-1] + x[0..i-1] +"ay"
+
+	end
+end 
+
+
+def translate (word)
+	if word.length <= 0 
+		return ""
+	end 
+
+	words = word.split
+	translated = ""
+
+
+	for x in words
+		#find how many consanants are there before a vowel appears 
+		num_cons = 0
+
+		x.split("").each do |i|
+			if (['a','e','i','o','u'].include? i)
+				break
+			else
+				num_cons +=1
+			end
+		end 
+
+		case num_cons
+		when 0
+			translated += x + "ay "
+
+		else
+			translated += doesQUexist(x,num_cons) + " "
+		end
+		
+
+	end
+
+	return translated.strip
+
+end
+
+puts translate("apple") 
