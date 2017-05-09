3f7ed470ba32ddfc35d86256a0fb07b2b664f77c
rjasi <venusara@gmail.com>
2017-05-09 15:36:27 -0400

    Finished timer

--- learn_ruby/06_timer/timer.rb
+++ learn_ruby/06_timer/timer.rb
@@ -1,3 +1,19 @@
+
 class Timer
-  #write your code here
+  def initialize 
+  	@seconds = 0
+  end
+
+  def seconds=(s)
+  	@seconds=s
+  end
+
+  def seconds
+  	return @seconds
+  end 
+
+
+  def time_string  	
+	return Time.at(@seconds).utc.strftime("%H:%M:%S") 
+  end 
 end
