6e2717012502e7238853ea35c478d9d7f5a670fc
rjasi <venusara@gmail.com>
2017-05-09 15:06:15 -0400

    Finished books

--- learn_ruby/05_book_titles/book.rb
+++ learn_ruby/05_book_titles/book.rb
@@ -1,3 +1,40 @@
 class Book
+
+	#This method properly capitalizes a title of a book
+	def titleize(a)
+		words = a.split(" ")
+		ans = ""
+		count = 0
+
+		for x in words
+			
+			if ( ((["and", "or", "the", "over", "to", "the", "a", "but","in","of","an"]).include? x) && (count != 0) )
+				puts x.downcase
+				ans += x.downcase
+			else
+				ans += x.capitalize
+			end
+			ans+= " "
+			count+=1
+		end
+
+		return ans.strip
+	end
+
+	def initialize
+		@title = ""
+	end 
+
+	def title=(title)
+		@title = titleize(title)
+	end
+
+	def title
+		return 	@title 
+	end
+
+
+
 # write your code here
 end
+
